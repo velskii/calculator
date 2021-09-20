a01753a6f44f7b99e67ab068d11f1d7838822b7a
velskii <posixie@gmail.com>
2021-09-20 14:55:06 -0400

    init commit

---
+++ calculator/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Jerry on 2021-09-19.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func zeroButton(_ sender: Any) {
+    }
+    
+    @IBAction func oneButton(_ sender: UIButton) {
+    }
+    @IBAction func twoButton(_ sender: UIButton) {
+    }
+    @IBAction func threeButton(_ sender: UIButton) {
+    }
+}
+
