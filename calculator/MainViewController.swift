b60fc1c6e121f1402efc138eb75218756700c810
velskii <posixie@gmail.com>
2021-09-24 17:15:35 -0400

    redesign the layout and try to add borders to the components

--- calculator/MainViewController.swift
+++ calculator/MainViewController.swift
@@ -1,13 +1,114 @@
 /*
- Filename: MainViewController.swift
- Author: Feiliang Zhou(Greg)
- StudentId: 301216989
- Data: September 20, 2021
- App description: simple calculator
- Version: 1.0
+ *Author            : Feiliang Zhou(Greg)
+ *StudentId         : 301216989
+ *Data              : September 20, 2021
+ *App description   : simple calculator
+ *Version           : 1.0
 */
 
 import UIKit
+
+/*
+ * I google this to get the buttons and lables with borders, it didn't show the borders in the interface builder, but it works in the emulator.
+ */
+@IBDesignable
+class DesignableView: UIView {
+}
+
+@IBDesignable
+class DesignableButton: UIButton {
+}
+
+@IBDesignable
+class DesignableLabel: UILabel {
+}
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+}
 
 class MainViewController: UIViewController {
 
